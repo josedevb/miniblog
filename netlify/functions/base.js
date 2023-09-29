d716a051a81a6ba981b95845f568cf255e9c3a50
Jose Barrios <josedbarrios7@gmail.com>
2023-09-29 14:30:42 -0500

    netlify setup

---
+++ netlify/functions/base.js
@@ -0,0 +1,20 @@
+const Airtable = require('airtable');
+
+const db = new Airtable({
+  apiKey: process.env.AIRTABLE_TOKEN
+}).base(process.env.AIRTABLE_BASE_ID)
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true
+}
+
+exports.handler = async function (evt) {
+  const articles = await db('articles').select().all()
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({ articles })
+  }
+}
