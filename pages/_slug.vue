14e2f3e1fb6aad2447cc0a741492b3ff1b4e2856
Jose Barrios <josedbarrios7@gmail.com>
2023-10-02 14:27:51 -0500

    nuxt util for comment

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -67,10 +67,13 @@ export default {
   },
   methods: {
     async createComment(comment) {
+      this.$nuxt.$loading.start();
       const url = location.hostname === "localhost" ? 'http://localhost:9999' : 'https://nuxt-miniblog-course.netlify.app'
-      await fetch(`${url}/.netlify/functions/comment?articles=${this.article._id}`, {
+      await fetch(`${url}/.netlify/functions/comment?article=${this.article._id}`, {
         method: 'post', body: JSON.stringify(comment)
       })
+      this.$nuxt.refresh();
+      this.$nuxt.$loading.finish();
     }
   }
 }
